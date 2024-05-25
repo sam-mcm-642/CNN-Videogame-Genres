a1439d93bc27328754cb2ffd4d09a05d603c40cc
sam-mcm-642 <sammcmanagan@icloud.com>
2024-05-25 16:18:46 +0200

    Finalizing code, adding report

--- Assignment 2/Library.py
+++
@@ -1,8 +0,0 @@
-
-import keras
-import numpy as np
-import sklearn as skl
-import pandas as pd
-import tensorflow as tf
-import json
-import PIL.Image as Image
---
+++ Library.py
@@ -0,0 +1,8 @@
+
+import keras
+import numpy as np
+import sklearn as skl
+import pandas as pd
+import tensorflow as tf
+import json
+import PIL.Image as Image
